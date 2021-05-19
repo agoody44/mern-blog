ba662dc046b5cf3a50a52a22206f8b4f2f98f8f1
“Alyssa <agoody44@gmail.com>
2021-05-19 00:36:42 -0400

    adds mongodb and .env and sets up post route

--- server/index.js
+++ server/index.js
@@ -3,3 +3,27 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/post.js';
+
+import dotenv  from "dotenv"
+dotenv.config()
+
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on ${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
