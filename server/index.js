677e9d468f665c9701a628900912e40ff09e6aa6
“Alyssa <agoody44@gmail.com>
2021-05-24 20:30:37 -0400

    mongo WIP

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 import postRoutes from './routes/post.js';
 
-import dotenv  from "dotenv"
+import dotenv  from "dotenv";
 dotenv.config()
 
 
@@ -13,8 +13,8 @@ const app = express();
 
 
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
@@ -24,10 +24,11 @@ app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on ${PORT}`)))
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
+
 
 mongoose.set('useFindAndModify', false);
